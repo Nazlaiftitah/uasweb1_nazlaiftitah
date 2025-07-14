4724780db65a1feba3199cd440047468be6013bc
Nazlaiftitah <iftitahnazla@gmail.com>
2025-07-14 22:59:37 +0700

    public: konfigurasi rewrite rules untuk serve assets dari folder assets/

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,15 @@
-<h1>About Me</h1>
-<p>Halo, nama Saya <?= $data['nama'] ?>, umur saya <?= $data['umur'] ?> tahun, sata adalah seorang
-    <?= $data['pekerjaan'] ?>.</p>
+<div class="container my-5">
+    <div class="row align-items-center">
+        <div class="col-md-4 text-center mb-4 mb-md-0">
+            <img src="<?= BASEURL ?>/img/nazla.jpeg" alt="Foto Profil" class="img-fluid rounded-circle shadow"
+                style="max-height: 200px;">
+        </div>
+        <div class="col-md-8">
+            <h1 class="display-6 fw-semibold">About Me</h1>
+            <p class="lead">
+                Halo, nama saya <strong><?= $data['nama'] ?></strong>, umur saya <strong><?= $data['umur'] ?></strong>
+                tahun, saya adalah seorang <strong><?= $data['pekerjaan'] ?></strong>.
+            </p>
+        </div>
+    </div>
+</div>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -7,9 +7,9 @@
     <link rel="stylesheet" href="<?= BASEURL; ?>/css/bootstrap.css">
     <title>Halaman <?= $data['judul'] ?></title>
 </head>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Web Nazla</a>
+        <a class="navbar-brand" href="<?= BASEURL; ?>">Web Nazla</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -17,17 +17,12 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="<?= BASEURL ?>">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
+                    <a class="nav-link" href="<?= BASEURL; ?>/about">About</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                </li>
+
             </ul>
         </div>
     </div>
