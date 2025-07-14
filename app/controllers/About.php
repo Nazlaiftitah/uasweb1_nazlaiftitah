2eced31736c9e2d74b3b25fcb513b2ed8e12a9bc
Nazlaiftitah <iftitahnazla@gmail.com>
2025-07-14 20:52:05 +0700

    controller: implementasikan HomeController contoh

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -2,8 +2,12 @@
 
 class About
 {
-    public function index()
+    public function index($nama, $pekerjaan)
     {
-        echo "About/index";
+        echo "Halo, Nama Saya $nama, saya adalah seorang $pekerjaan";
+    }
+    public function page()
+    {
+        echo "About/page";
     }
 }
