5809664a95521967953eff72546fe395083c9ec1
Nazlaiftitah <iftitahnazla@gmail.com>
2025-07-15 04:45:30 +0700

    search: tambahkan form pencarian di view

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -58,4 +58,12 @@ class Mahasiswa_model
 
         return $this->db->execute() ? $this->db->rowCount() : 0;
     }
+
+    public function cariDataMahasiswa($keyword)
+    {
+        $query = "SELECT * FROM " . $this->table . " WHERE nama LIKE :keyword";
+        $this->db->query($query);
+        $this->db->bind(':keyword', '%' . $keyword . '%');
+        return $this->db->resultSet();
+    }
 }
